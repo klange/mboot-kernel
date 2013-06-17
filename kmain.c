9a44e97ac9b3c99cb8b8c3a60ae3af3e680c2efb
Kevin Lange <klange@dakko.us>
2013-06-17 13:42:56 -0700

    Hello world (really now)

--- kmain.c
+++ kmain.c
@@ -1,3 +1,12 @@
+unsigned short * vga_text = (unsigned short *)0xB8000;
+char msg[] = "Welcome to YelpOS!";
+
 void kernel_main() {
-	/* do nothing */
+	char * i = msg;
+	unsigned short * c = vga_text;
+	while (*i != 0) {
+		*c = (*i | (15 | (0 << 4)) << 8);
+		i++;
+		c++;
+	}
 }
